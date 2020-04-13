00194ef6a1f6f6261866933ef9454b71b920c561
Isaac <isaac@valr.com>
2020-04-13 16:03:49 +0200

    Updates

---
+++ precache-manifest.b1ea335f11df5f7f47d3bab1665c9c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9cf71627e314d555f00d1b24a22bbee",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cbfe4f087b1d7bcc6f7d",
+    "url": "/monsters-rolodex/static/css/main.aabf7b27.chunk.css"
+  },
+  {
+    "revision": "1d6d326f4470ed67fe85",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfe4f087b1d7bcc6f7d",
+    "url": "/monsters-rolodex/static/js/main.b93c2386.chunk.js"
+  },
+  {
+    "revision": "aa1aeb4ed7e62e71a38e",
+    "url": "/monsters-rolodex/static/js/runtime-main.2292ee2f.js"
+  }
+]);
